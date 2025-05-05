b481fdbb31343dd9380e527d6a9cfe5d840aaf79
Ahmed Abdel-Nasser <ahmedabdulnasser.ai@gmail.com>
2025-05-05 23:48:33 +0300

    fix cors issues

--- server/index.js
+++ server/index.js
@@ -9,14 +9,16 @@ const authRoutes = require("./routes/auth.routes");
 const userRoutes = require("./routes/user.routes");
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: "http://localhost:5173/",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173/",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//     credentials: true,
+//   })
+// );
+app.options("*", cors());
+
 app.use("/api/auth", authRoutes);
 app.use("/api", apiRoutes);
 app.use("/api/users", userRoutes);
