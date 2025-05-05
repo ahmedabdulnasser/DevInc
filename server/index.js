3c08b1b37807dc35ca83fa9d2c602976c88a9271
Ahmed Abdel-Nasser <ahmedabdulnasser.ai@gmail.com>
2025-05-05 23:51:58 +0300

    fix cors issues

--- server/index.js
+++ server/index.js
@@ -18,6 +18,14 @@ app.use(express.json());
 //   })
 // );
 app.options("*", cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api", apiRoutes);
